bb881d8d84afd9907eb080c24f509ce8f81eada7
houshengwu <67622217+houshengwu@users.noreply.github.com>
2024-05-06 15:11:50 +0800

    DDDDDD

---
+++ auto_run.bat
@@ -0,0 +1,4 @@
+@echo off
+
+rem 打开 PowerShell 并执行命令
+start powershell -NoExit -Command "cd C:\Users\10101\all_python_project\文字识别\text_ocr\Scripts; ./activate;cd C:\Users\10101\all_python_project\文字识别\text_ocr_app;python train.py;"
